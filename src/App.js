304eb74b209a35cdad522443e5c0e4cd25899039
mdflynn <mdflynn34@outlook.com>
2020-12-01 17:37:41 -0700

    Add nav and movie container components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Nav from './Nav'
+import React, { Component } from 'react';
+import movieData from './movieData'
+import MovieContainer from './MovieContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      movies: movieData.movies
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <MovieContainer movies={this.state.movies}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
