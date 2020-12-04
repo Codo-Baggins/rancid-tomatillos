54ea6b3c7b961336c7afa9973156267e4c939334
mdflynn <mdflynn34@outlook.com>
2020-12-03 17:55:27 -0700

    Add Movie component tests

---
+++ src/Movie/Movie.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { screen, render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Movie from './Movie.js'
+
+describe('Movie', () => {
+  it('should render correctly', () => {
+    render(<Movie
+      movieSelected={{
+        id: 694919,
+        poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        title: "Money Plane",
+        average_rating: 6.666666666666667,
+        release_date: "2020-09-29"
+      }}
+    />)
+
+    expect(screen.getByText('Money Plane')).toBeInTheDocument()
+    //expect(screen.getByText('2020-09-29')).toBeInTheDocument()
+    expect(screen.getByText(6.666666666666667)).toBeInTheDocument()
+    expect(screen.getByText("Some Details")).toBeInTheDocument()
+  })
+})
