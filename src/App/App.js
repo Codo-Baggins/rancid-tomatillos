0b0fad42e103b50199e2f9c125552060284633b5
mdflynn <mdflynn34@outlook.com>
2020-12-03 17:55:11 -0700

    Add comments for research

--- src/App/App.js
+++ src/App/App.js
@@ -10,17 +10,26 @@ class App extends Component {
     super()
     this.state = {
       movies: movieData.movies,
-      movieSelected: {} 
+      movieSelected: {}
     }
   }
-
+//integration test, tests the methods
   handleClick = (index) => {
+    //fetch her for single movie
+   // .then(data => this.setState({movieSelected: data}))
     this.setState({ movieSelected: this.state.movies[index] })
   }
 
   handleSubmit = () => {
     this.setState( { movies: movieData.movies, movieSelected: {} })
   }
+
+
+//TESTS: More/less? How to do integration test?
+
+//click movie card, see if Movie is there
+//on Movie, click home to see if MovieContainer is there
+//test home button on home page still shows home page
 
   render() {
     return (
@@ -29,7 +38,7 @@ class App extends Component {
         { Object.keys(this.state.movieSelected).length ? <Movie movieSelected={ this.state.movieSelected } /> : <MovieContainer
           movies={this.state.movies}
           handleClick={this.handleClick}
-        /> } 
+        /> }
       </div>
     );
   }
