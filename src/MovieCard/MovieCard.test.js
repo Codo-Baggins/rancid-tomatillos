245efd8dde068272079bf398bd0febbc6c6a3dc0
mdflynn <mdflynn34@outlook.com>
2020-12-03 17:56:04 -0700

    Format test file

--- src/MovieCard/MovieCard.test.js
+++ src/MovieCard/MovieCard.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { screen, render, fireEvent } from '@testing-library/react'
-import { getByText } from '@testing-library/dom'
+import { getByText, getByAltText } from '@testing-library/dom'
+//do we need this for testing?
 import '@testing-library/jest-dom'
 import MovieCard from './MovieCard.js'
 
@@ -37,8 +38,10 @@ describe('MovieCard', () => {
       //click on outer area too
     fireEvent.click(screen.getByText('Cheech and Chong'))
     expect(mockHandleClick).toHaveBeenCalledWith(0)
+
     fireEvent.click(screen.getByAltText('Cheech and Chong'))
     expect(mockHandleClick).toHaveBeenCalledWith(0)
+
     fireEvent.click(screen.getByText('2020-09-29'))
     expect(mockHandleClick).toHaveBeenCalledWith(0)
   })
