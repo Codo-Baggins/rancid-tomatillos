3c90ddf89658d2fc14fced4ef3054b432fc6fdc6
mdflynn <mdflynn34@outlook.com>
2020-12-05 13:23:17 -0700

    Add error handling to state

--- src/App/App.js
+++ src/App/App.js
@@ -4,47 +4,40 @@ import React, { Component } from 'react';
 //import movieData from '../movieData';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import Movie from '../Movie/Movie';
-import { callApi, callSingleApi } from '../callApis'
+import { callApi, callSingleApi } from '../callApis/callApis'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       movies: [],
-      movieSelected: {}
+      movieSelected: {},
+      error: null
     }
   }
   //integration test, tests the methods
   handleClick = (id) => {
-    console.log(id)
     //fetch her for single movie
     // .then(data => this.setState({movieSelected: data}))
     callSingleApi(id)
       .then(data => this.setState({ movieSelected: data.movie }))
-      .then(error => console.log(error))
-
+      .catch(error => this.setState({ error }))
   }
 
-  handleSubmit = () => {
-    //setState to fetch data?
+  returnHome = () => {
     this.setState({ movieSelected: {} })
   }
 
   componentDidMount() {
     callApi()
       .then(data => this.setState({ movies: data.movies }))
-      .then(error => console.log('error'))
+      .catch(error => this.setState({ error }))
   }
-  //TESTS: More/less? How to do integration test?
-
-  //click movie card, see if Movie is there
-  //on Movie, click home to see if MovieContainer is there
-  //test home button on home page still shows home page
 
   render() {
     return (
       <div className="App">
-        <Nav handleSubmit={ this.handleSubmit } />
+        <Nav returnHome={ this.returnHome } />
         { Object.keys(this.state.movieSelected).length ? <Movie movieSelected={ this.state.movieSelected } /> : <MovieContainer
           movies={ this.state.movies }
           handleClick={ this.handleClick }
