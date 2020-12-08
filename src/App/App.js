56ebf11f4152e7042b6db4a29b31681e2c785934
mdflynn <mdflynn34@outlook.com>
2020-12-08 15:39:13 -0700

    Add Router and video data

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,20 @@
 import './App.scss';
 import Nav from '../Nav/Nav';
 import React, { Component } from 'react';
-//import movieData from '../movieData';
 import MovieContainer from '../MovieContainer/MovieContainer';
 import Movie from '../Movie/Movie';
-import { callApi, callSingleApi } from '../callApis'
-
+import {
+  callApi,
+  callSingleApi,
+  //import get video api
+  callSingleApiVideo
+} from '../callApis'
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch
+} from 'react-router-dom';
 
 class App extends Component {
   constructor() {
@@ -14,16 +23,39 @@ class App extends Component {
       movies: [],
       movieSelected: {},
       filteredMovies: [],
+      //add video to state to display on movie page
+      movieSelectedVideo: '',
       error: null
     }
   }
 
   handleClick = (id) => {
-    callSingleApi(id)
-    .then(data => this.setState({ movieSelected: data.movie }))
-    .catch(error => this.setState({ error }))
+    this.getSingleApi(id);
+    this.getSingleApiVideo(id);
   }
 
+  //split methods to get api then add to handleClick
+  getSingleApi = (id) => {
+    callSingleApi(id)
+      .then(data => this.setState({ movieSelected: data.movie }))
+      .catch(error => this.setState({ error }))
+  }
+
+  getSingleApiVideo = (id) => {
+    callSingleApiVideo(id)
+      .then(data => this.setState({ movieSelectedVideo: this.filterVideoTypes(data.videos) }))
+      .catch(error => this.setState({ error }))
+  }
+
+  //return url of video trailer, filter to find trailer (multiple types)
+
+  filterVideoTypes = (types) => {
+    //non stop running, why?
+    const displayVideo = types.find(video => video.type === "Trailer");
+    return `https://www.youtube.com/watch?v=${ displayVideo.key }/videos`
+  }
+
+  //can delete this w/ react router, state will overwrite itself with each click
   returnHome = () => {
     this.setState({
       movieSelected: {},
@@ -60,15 +92,44 @@ class App extends Component {
   }
 
   render() {
-    //do calc on what we want to render
+    //how to incorporate form search into React Router?
+    //filter w/ each keystroke
+    //updating url with this?
     return (
-      <div className="App">
-        <Nav
-          returnHome={ this.returnHome }
-          filterMovies={ this.filterMoviesByTitle }
-        />
-        {this.generateView()}
-      </div>
+      <Router>
+        <div className="App">
+
+          {/* Route to home with the button */ }
+          <Nav
+            returnHome={ this.returnHome }
+            filterMovies={ this.filterMoviesByTitle }
+          />
+          <Switch>
+            <Route exact path='/'
+              render={ () => {
+                return <MovieContainer
+                  movies={ this.state.movies }
+                  handleClick={ this.handleClick }
+                />
+              } } />
+
+            <Route path="/movie/:id"
+              render={ ({ match }) => {
+                // logging 3 million times...
+                console.log(this.state.movieSelectedVideo);
+
+                this.handleClick(parseInt(match.params.id))
+                return <Movie
+                  movieSelected={ this.state.movieSelected }
+                  //need to pass down and add to Movie component
+                  movieTrailer={ this.state.movieSelectedVideo }
+                />
+              } }
+            />
+
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
