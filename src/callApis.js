ef4625270b7388bf636f4c0a66efbf975367734a
mdflynn <mdflynn34@outlook.com>
2020-12-08 15:38:36 -0700

    Add api to get video data

--- src/callApis.js
+++ src/callApis.js
@@ -8,7 +8,14 @@ const callSingleApi = (id) => {
     .then(response => response.json())
 }
 
+//get single movie video
+const callSingleApiVideo = (id) => {
+  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
+    .then(response => response.json())
+}
+
 export {
   callApi,
-  callSingleApi
+  callSingleApi,
+  callSingleApiVideo
 }
