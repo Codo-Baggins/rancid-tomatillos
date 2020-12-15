e37365e1bbc75bd9ca45c5a72676feded359b21a
Joe Lopez <lopez.4.joseph@gmail.com>
2020-12-14 17:36:16 -0700

    Format the files

--- src/callApis.js
+++ src/callApis.js
@@ -1,21 +1,20 @@
 const callApi = () => {
-  console.log("Real callAPI was invoked")
-  return fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
-    .then(response => response.json())
-}
+  console.log("Real callAPI was invoked");
+  return fetch(
+    "https://rancid-tomatillos.herokuapp.com/api/v2/movies"
+  ).then((response) => response.json());
+};
 
 const callSingleApi = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
-    .then(response => response.json())
-}
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`
+  ).then((response) => response.json());
+};
 
 const callSingleApiVideo = (id) => {
-  return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)
-    .then(response => response.json())
-}
+  return fetch(
+    `https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`
+  ).then((response) => response.json());
+};
 
-export {
-  callApi,
-  callSingleApi,
-  callSingleApiVideo
-}
+export { callApi, callSingleApi, callSingleApiVideo };
