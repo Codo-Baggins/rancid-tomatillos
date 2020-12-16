a8640812bce45da49da8c04b561db10c41b7bd60
Joe Lopez <lopez.4.joseph@gmail.com>
2020-12-15 17:18:29 -0700

    Edit the path

--- src/App/App.js
+++ src/App/App.js
@@ -90,7 +90,7 @@ class App extends Component {
           <Switch>
             <Route
               exact
-              path="/"
+              path="/rancid-tomatillos/"
               render={() => {
                 return searchResults;
               }}
